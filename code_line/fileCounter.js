2247618d2222bd12423739066c2db290c0b1d521
孙涛 <2605323669@qq.com>
2024-10-27 16:52:00 +0800

    实现统计注释行数 现存问题：注释内包含//和代码中包含//会计数

--- code_line/fileCounter.js
+++ code_line/fileCounter.js
@@ -1,14 +1,99 @@
 const fs = require("fs")
-const path = require("path")
+/**
+ * 用来统计代码行量//
+ * @param {*} dirPath 
+ */
+const commentPatterns = {
+    js: {
+        singleLine: /\/\/.*$/gm,
+        multiLine: /\/\*[\s\S]*?\*\//g,
+        regex:/^\/\/.*/
+    },
+    java: {
+        singleLine: /\/\/.*$/gm,
+        multiLine: /\/\*[\s\S]*?\*\//g
+    },
+    py: {
+        singleLine: /#.*$/gm,
+        multiLine: /(['"]{3})[\s\S]*?\1/g
+    },
+    xml: {
+        multiLine: /<!--[\s\S]*?-->/g
+    },
+    html: {
+        singleLine: /\/\/.*$/gm,
+        multiLine: /<!--[\s\S]*?-->|\/\*[\s\S]*?\*\//g
+    },
+    lua: {
+        singleLine: /--.*$/gm,
+        multiLine: /--\[\[[\s\S]*?\]\]/g
+    },
+    cs: {
+        singleLine: /\/\/.*$/gm,
+        multiLine: /\/\*[\s\S]*?\*\//g
+    },
+    php: {
+        singleLine: /\/\/.*$|#.*$/gm,
+        multiLine: /\/\*[\s\S]*?\*\//g
+    },
+    css: {
+        multiLine: /\/\*[\s\S]*?\*\//g
+    }
+};
 
-filePath = "F:\\node_pkg"
+// 根据文件类型选择适当的注释正则表达式
+function getCommentPatternsByExtension(fileName) {
+    // 提取文件后缀
+    const fileExtension = fileName.slice(fileName.lastIndexOf('.') + 1);
+    // 查找对应的注释配置
+    const patterns = commentPatterns[fileExtension];
+    if (!patterns) {
+        console.error(`未找到与.${fileExtension}文件匹配的注释模式`);
+        return {};
+    }
+    return patterns;
+}
+
 function countLinesInDirectory(dirPath) {
+    let emptyLines = 0;
+    let codeLines = 0;
+    let commentLineCount = 0;//注释行数
     // 读取文件并统计行数  
     fs.readFile(dirPath, 'utf8', (err, data) => {
         if (err) {
             return console.error(`无法读取文件: ${err}`);
         }
         const lineCount = data.split('\n').length;
-        console.log(`文件: ${dirPath} 行数: ${lineCount}`);
+        const lines = data.split('\n');
+
+        const patterns = getCommentPatternsByExtension(dirPath);
+
+        lines.forEach(line => {
+            //trim()方法将空白字符转换为空字符串
+            if (line.trim() === '') emptyLines++;
+            else {
+                codeLines++;
+            }
+        });
+
+        单行注释行数统计
+        if (patterns.singleLine) {
+            const singleLineComments = data.match(patterns.singleLine) || [];
+            commentLineCount += singleLineComments.length;
+            console.log(`${singleLineComments}`);
+        }
+
+        // 多行注释行数统计
+        if (patterns.multiLine) {
+            const multiLineComments = data.match(patterns.multiLine) || [];
+            multiLineComments.forEach(comment => {
+                commentLineCount += comment.split('\n').length;
+            });
+            
+        }
+        console.log(`文件: ${dirPath} 总行数: ${lineCount}  空行数: ${emptyLines} 注释行数: ${commentLineCount}`);//
     });
+}
+module.exports = {
+    countLinesInDirectory
 }
