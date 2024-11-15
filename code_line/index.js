b47f2f57795ea09e1a159a12785a362d9a1de4cf
孙涛 <2605323669@qq.com>
2024-11-15 16:07:09 +0800

    命令行参数处理优化及错误检查增强

--- code_line/index.js
+++ code_line/index.js
@@ -1,19 +1,21 @@
 const readDirectory = require("./readDirectory");
 const fs = require("fs")
 const promptSync = require('prompt-sync')();
+
 // 可以通过执行命令传入目录参数  
 const params = process.argv.slice(2);  // 使用 slice 而不是 splice，第二个参数是传入的目录路径
-const showSummary = params.includes("--summary");
 const filePath = params[0];
-console.log("请输入要排除的目录或文件（输入 'done' 结束）：");
+
+if (params.length < 1) {
+    console.error('用法: node index <路径> [--summary]');
+    process.exit(1);
+}
+
+let showSummary = false;
 let userExcludeDirs = [];
 let input = "";
-do {
-    input = promptSync();
-    if (input !== "done") {
-        userExcludeDirs.push(input);
-    }
-} while (input !== "done");
+
+//验证路径
 try {
     if (!filePath || !fs.existsSync(filePath) || !fs.statSync(filePath).isDirectory()) {
         console.error('没有提供路径或路径不存在！请检查！');
@@ -23,5 +25,26 @@ try {
     console.error('路径验证时出现错误:', error.message);
     process.exit(1);
 }
-console.log(userExcludeDirs);
+
+// 处理可选的 --summary 标志和其他参数
+
+if (params.length > 1) {
+    if (params[1] !== "--summary") {
+        console.error('用法: node index <路径> [--summary]');
+        process.exit(1);
+    } else if (params.length == 2) {
+        showSummary = true;
+    } else {
+        console.error('用法: node index <路径> [--summary] (不应有其他参数)');
+        process.exit(1);
+    }
+}
+
+console.log("请输入要排除的目录或文件（输入 'done' 结束）：");
+do {
+    input = promptSync();
+    if (input !== "done") {
+        userExcludeDirs.push(input);
+    }
+} while (input !== "done");
 readDirectory.calculateTotalLines(filePath, showSummary, userExcludeDirs);
